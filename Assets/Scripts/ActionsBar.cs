c799afaf62177176bb09f0b7f004a21c2257b96d
WillemGillis <willem.gillis@imec.be>
2019-10-09 16:43:16 +0200

    PLDN-35 Finish timeline styling

---
+++ Assets/Scripts/ActionsBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ActionsBar : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject m_Button;
+
+	void Start()
+	{
+		for (int i = 0; i < 15; i++)
+		{
+			GameObject.Instantiate(m_Button, transform);
+		}
+	}
+
+}
