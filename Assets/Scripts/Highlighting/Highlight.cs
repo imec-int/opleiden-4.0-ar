a431a46e68689514e32a01260427ca85107a9af6
MatthiasVL <matthias.vanlierde@imec.be>
2019-10-10 16:58:42 +0200

    PLDN-46 Added GUI Highlight Element Prefab and 3D Canvas to the scene

---
+++ Assets/Scripts/Highlighting/Highlight.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highlight : MonoBehaviour
+{
+    // Unity-facing variables
+    // Useful children
+    [SerializeField]
+    private Image _BGImage;
+    [SerializeField]
+    private Button _Button;
+    [SerializeField]
+    private TextMeshProUGUI[] _PositionLabels = new TextMeshProUGUI[4];
+
+    // Code interface
+    public Vector3 AssociatedWSPoint
+    {
+        get;
+        set;
+    }
+
+    private uint _UID;
+    public uint UID
+    {
+        get
+        {
+            return _UID;
+        }
+    }
+
+    // Boilerplate
+    void Awake()
+    {
+        _Button.onClick.AddListener(onButtonClicked);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // The meat
+    //=============
+    // Update is called once per frame
+    void Update()
+    {
+        // Track 3D Position
+
+    }
+
+    private void onButtonClicked()
+    {
+        // Spawn the actions
+    }
+}
