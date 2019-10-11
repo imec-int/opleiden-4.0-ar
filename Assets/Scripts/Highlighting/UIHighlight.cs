2622364c892e862aa4f0d948439a239ec1fa27ca
MatthiasVL <matthias.vanlierde@imec.be>
2019-10-11 10:14:52 +0200

    PLDN-46 Refactoring files to make the naming convention more stringent, added highlight anchor class, prefabs to project and objects to the pump; Added highlightcontainer to spawn and manage highlights on the model

---
+++ Assets/Scripts/Highlighting/UIHighlight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHighlight : MonoBehaviour
+{
+    // Unity-facing variables
+    // Useful children
+    [SerializeField]
+    private Image _BGImage = null;
+    [SerializeField]
+    private Button _Button = null;
+    [SerializeField]
+    private TextMeshProUGUI[] _ActionPositionLabels = new TextMeshProUGUI[4];
+
+    [SerializeField]
+    private float minSize = 0.1f;
+    [SerializeField]
+    private float maxSize = 1.0f;
+
+    private RectTransform _RectTransform = null;
+
+    public Vector3 DEBUGPos = Vector3.zero;
+
+
+    // Boilerplate
+    void Awake()
+    {
+        _Button.onClick.AddListener(onButtonClicked);
+        _RectTransform = this.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        var cameraSpacePos = Camera.main.worldToCameraMatrix*this.transform.position;
+        DEBUGPos = cameraSpacePos;
+        _RectTransform.localScale = Vector3.one*Mathf.Lerp(maxSize,minSize,cameraSpacePos.z);
+    }
+
+    private void onButtonClicked()
+    {
+        //Show the menu
+    }
+}
---
+++ Assets/Scripts/Highlighting/UIHighlightContainer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHighlightContainer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _3DModel = null;
+    [SerializeField]
+    private UIHighlight _UIHighlightPrefab = null;
+
+    private List<UIHighlight> _UIHighlightInstancesList = new List<UIHighlight>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaceHighlights(_3DModel);
+        // debug
+        //Reset(_3DModel);
+    }
+
+    public void Reset(GameObject newmodel = null)
+    {
+        foreach(var highlight in _UIHighlightInstancesList)
+        {
+            highlight.StopAllCoroutines();
+            GameObject.Destroy(highlight.gameObject);
+        }
+        _UIHighlightInstancesList.Clear();
+
+        if (newmodel)
+            PlaceHighlights(newmodel);
+    }
+
+    public void SetVisibility(bool visibile)
+    {
+        foreach(var highlight in _UIHighlightInstancesList)
+        {
+            highlight.gameObject.SetActive(visibile);
+        }
+    }
+
+    private void PlaceHighlights(GameObject model)
+    {
+        var anchors = model.GetComponentsInChildren<HighlightAnchor>();
+        Debug.Assert(anchors.Length > 0, $"Failed to retrieve anchors for the provided gameobject {model.name}");
+
+        foreach(var anchor in anchors)
+        {
+            var newObj = GameObject.Instantiate(_UIHighlightPrefab, anchor.transform.position, Quaternion.Euler(0,0,0));
+            // TODO: Additional setup from anchor           
+            _UIHighlightInstancesList.Add(newObj);
+            // Set up new object
+            newObj.transform.SetParent(this.transform,true);
+        }        
+    }
+}
