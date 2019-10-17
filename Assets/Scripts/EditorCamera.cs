5374f591ee1b42fc6cfbb18e910d4546d30e60c0
WillemGillis <willem.gillis@imec.be>
2019-10-17 09:34:59 +0200

    PLDN-47: Add editor only script for debugging

---
+++ Assets/Scripts/EditorCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorCamera : MonoBehaviour
+{
+#if UNITY_EDITOR
+	[SerializeField]
+	private Vector3 _startPosition;
+
+	[SerializeField]
+	private Vector3 _startRotation;
+
+	[SerializeField]
+	private GameObject _installation;
+
+	private void Awake()
+	{
+		_installation.SetActive(true);
+		transform.SetPositionAndRotation(_startPosition, Quaternion.Euler(_startRotation));
+		Destroy(this);
+	}
+#endif
+}
