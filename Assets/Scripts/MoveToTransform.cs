05c524196a6cd28bd9c441fdb1982b6181b25388
WillemGillis <willem.gillis@imec.be>
2019-10-18 15:45:27 +0200

    PLDN-47: Improve editor AR setup

---
+++ Assets/Scripts/MoveToTransform.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveToTransform : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _transform;
+
+	private void Awake()
+	{
+		_transform.SetPositionAndRotation(transform.position, transform.rotation);
+	}
+}
