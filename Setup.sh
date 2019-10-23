6197fa4c704219ba6ef91ff40cef786b8923266d
WillemGillis <willem.gillis@imec.be>
2019-10-23 11:18:12 +0200

    PLDN-56: Put omnisharp configuration inside setup script

--- Setup.sh
+++ Setup.sh
@@ -1,14 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-# Set custom git hooks folder
+# Setup GIT
+
+## Set custom git hooks folder
 git config core.hooksPath .githooks
 
-# Create git lfs hooks
+## Create git lfs hooks
 git lfs install
 
-# Move conflicting git lfs hooks to subdirectory
+## Move conflicting git lfs hooks to subdirectory
 mv ./.githooks/post-checkout ./.githooks/post-checkout.d/git-lfs
 mv ./.githooks/post-merge ./.githooks/post-merge.d/git-lfs
 
 cp ./.githooks/multiple-git-hooks ./.githooks/post-checkout
 cp ./.githooks/multiple-git-hooks ./.githooks/post-merge
+
+
+# Setup omnisharp
+
+if [[ $(uname -r) =~ Microsoft$ ]]; then
+  HOME=$(wslpath $(cmd.exe /C "echo %USERPROFILE%") | tr -d '\r')
+  
+  CONFIGHOME=$(cmd.exe /C "echo %USERPROFILE%") 
+  CONFIGHOME=$(echo $CONFIGHOME | tr '\\' '/' | tr -d '\r') 
+else
+  HOME=~
+  CONFIGHOME=~
+fi
+
+## Create omnisharp folder
+mkdir -p $HOME/.omnisharp
+
+## Create omnisharp configuration
+cat > $HOME/.omnisharp/omnisharp.json <<- EOM
+{
+	"RoslynExtensionsOptions": {
+		"enableAnalyzersSupport": true,
+		"LocationPaths": [
+			"$CONFIGHOME/.vscode/extensions/josefpihrt-vscode.roslynator-2.2.0/roslyn/common",
+			"$CONFIGHOME/.vscode/extensions/josefpihrt-vscode.roslynator-2.2.0/roslyn/analyzers",
+			"$CONFIGHOME/.vscode/extensions/josefpihrt-vscode.roslynator-2.2.0/roslyn/refactorings",
+			"$CONFIGHOME/.vscode/extensions/josefpihrt-vscode.roslynator-2.2.0/roslyn/fixes"
+		]
+	}
+}
+EOM
