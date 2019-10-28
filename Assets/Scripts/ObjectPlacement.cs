8ae4b5838cbc9b7630e776fc1f61e86ff9fec95f
WillemGillis <willem.gillis@imec.be>
2019-10-28 15:45:43 +0100

    PLDN-52: Open up object scale when using plane tracking

--- Assets/Scripts/ObjectPlacement.cs
+++ Assets/Scripts/ObjectPlacement.cs
@@ -8,37 +8,40 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARRaycastManager))]
 public class ObjectPlacement : MonoBehaviour
 {
-	private ARRaycastManager _RaycastManager;
-	private ARSessionOrigin _SessionOrigin;
+	private ARRaycastManager _raycastManager;
+	private ARSessionOrigin _sessionOrigin;
 
 	[SerializeField]
-	private Transform _Installation;
+	private Transform _installation;
+
+	[SerializeField]
+	private float _installationScale = 1;
 
 	private void Awake()
 	{
-		_SessionOrigin = GetComponent<ARSessionOrigin>();
-		_RaycastManager = GetComponent<ARRaycastManager>();
+		_sessionOrigin = GetComponent<ARSessionOrigin>();
+		_raycastManager = GetComponent<ARRaycastManager>();
 	}
 
 	// Start is called before the first frame update
 	void Update()
 	{
-		if (Input.touchCount == 0 || _Installation == null)
+		if (Input.touchCount == 0 || _installation == null)
 			return;
 
 		List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
-		Vector3 pos = Camera.main.WorldToViewportPoint(_Installation.position);
+		Vector3 pos = Camera.main.WorldToViewportPoint(_installation.position);
 
-		if (pos.x < 0 || pos.x > 1 || pos.y < 0 || pos.y > 1 || pos.z < 0 || _Installation.gameObject.activeSelf == false)
+		if (pos.x < 0 || pos.x > 1 || pos.y < 0 || pos.y > 1 || pos.z < 0 || _installation.gameObject.activeSelf == false)
 		{
-			if (_RaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+			if (_raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
 			{
-				_Installation.gameObject.SetActive(true);
+				_installation.gameObject.SetActive(true);
 				//_SessionOrigin.MakeContentAppearAt(_Installation, hits[0].pose.position);
 
-				_Installation.position = hits[0].pose.position;
-				_Installation.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+				_installation.position = hits[0].pose.position;
+				_installation.localScale = new Vector3(_installationScale, _installationScale, _installationScale);
 			}
 		}
 	}
