e1462305f6dc50574c03c2781fb3ff36de77ecd4
MatthiasVL <matthias.vanlierde@imec.be>
2020-02-05 12:55:15 +0100

    PLDN-157: Added more instructions Hiding info ribbon when panel is active

--- Assets/Scripts/UI/TopInfoRibbon.cs
+++ Assets/Scripts/UI/TopInfoRibbon.cs
@@ -6,6 +6,7 @@ using RotaryHeart.Lib.SerializableDictionary;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -14,14 +15,36 @@ namespace UI
 		protected static TopInfoRibbon _instance = null;
 		public static TopInfoRibbon Instance => _instance;
 
+		[SerializeField]
+		private InfoPanel _infoPanel;
+
 		private TextMeshProUGUI _textLabel;
 
 		protected void Awake()
 		{
 			_textLabel = this.GetComponentInChildren<TextMeshProUGUI>();
 			Assert.IsNotNull(_textLabel);
+			Assert.IsNotNull(_infoPanel);
 			_instance = this;
 			this.gameObject.SetActive(false);
+			_infoPanel.OnOpen  += Hide;
+			_infoPanel.OnClose += Show;
+		}
+
+		private void Show()
+		{
+			foreach(Graphic gra in this.GetComponentsInChildren<Graphic>())
+			{
+				gra.enabled = true;
+			}
+		}
+
+		private void Hide()
+		{
+			foreach(Graphic gra in this.GetComponentsInChildren<Graphic>())
+			{
+				gra.enabled = false;
+			}
 		}
 
 		public void SetLabelText(string text)
