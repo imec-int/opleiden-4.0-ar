69a6c3ee982b2d74abd0132f17e42483cf5a8527
MatthiasVL <matthias.vanlierde@imec.be>
2020-07-02 14:02:27 +0200

    PLDN-70: feature(actiontoanchor): * initial setup

---
+++ Assets/Scripts/UI/Highlighting/ActionToAnchorHighlighter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionToAnchorHighlighter : MonoBehaviour
+{
+	private LineRenderer _lineRenderer;
+
+	void Awake()
+	{
+        _lineRenderer = this.GetComponent<LineRenderer>();
+        Debug.Assert(_lineRenderer != null, "Failed to find linerenderer component");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
