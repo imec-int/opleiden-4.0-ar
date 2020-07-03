5a53f6fe49249f00c5de616d3a41aadc3d26b29a
MatthiasVL <matthias.vanlierde@imec.be>
2020-07-03 14:01:09 +0200

    PLDN-70: feat(arrow): * drawing GUI * Doing maths

--- Assets/Scripts/UI/Highlighting/ActionToAnchorHighlighter.cs
+++ Assets/Scripts/UI/Highlighting/ActionToAnchorHighlighter.cs
@@ -6,7 +6,15 @@ namespace UI
 {
 	public class ActionToAnchorHighlighter : MonoBehaviour
 	{
+		[SerializeField]
+		float _width = 3.0f;
+
 		private LineRenderer _lineRenderer;
+		private TimelineActionWidget _widget;
+		private GameObject _anchor;
+
+		[SerializeField]
+		private Texture _texture;
 
 		void Awake()
 		{
@@ -19,28 +27,58 @@ namespace UI
 			_lineRenderer.enabled = false;
 			_lineRenderer.useWorldSpace = true;
 		}
-
-		// Update is called once per frame
-		void Update()
+		void OnGUI()
 		{
-
+			if (_widget != null && _anchor != null)
+			{
+				Rect currentWidgetDrawPos = GetGUIRectFromRectTransform(_widget.GetComponent<RectTransform>());
+				Vector2 screenPosAnchor = CalculateScreenPosFromWorldPos(_anchor.transform.position);
+				float length = Vector2.Distance(currentWidgetDrawPos.position, screenPosAnchor);
+				currentWidgetDrawPos.height = -1 * length;
+				// rotate to face
+				GUI.Box(new Rect(screenPosAnchor, new Vector2(10.0f, 10.0f)), "");
+				var rotAngle = CalculateRotationAngle(currentWidgetDrawPos.position, screenPosAnchor);
+				GUIUtility.RotateAroundPivot(rotAngle, currentWidgetDrawPos.position);
+				GUI.DrawTexture(currentWidgetDrawPos, _texture);
+			}
 		}
 
-		public Rect GetScreenCoordinates(RectTransform uiElement)
+		#region  OnGUI Helpers
+		private static readonly int TOP_RIGHT_CORNER = 2;
+		private static readonly int TOP_LEFT_CORNER = 1;
+		Rect GetGUIRectFromRectTransform(RectTransform rectTransform)
 		{
-			var worldCorners = new Vector3[4];
-			uiElement.GetWorldCorners(worldCorners);
-			var result = new Rect(
-										worldCorners[0].x,
-										worldCorners[0].y,
-										worldCorners[2].x - worldCorners[0].x,
-										worldCorners[2].y - worldCorners[0].y);
-			return result;
+			var corners = new Vector3[4];
+			rectTransform.GetWorldCorners(corners);
+			// Halfway between the top left and top right
+			var x = Mathf.Lerp(corners[TOP_LEFT_CORNER].x, corners[TOP_RIGHT_CORNER].x, 0.5f);
+			var y = Mathf.Lerp(corners[TOP_LEFT_CORNER].y, corners[TOP_RIGHT_CORNER].y, 0.5f);
+			// Flip the Y-Coordinate... Because Unity cannot decide whether Y is up or down.
+			var startPos = new Vector2(x, Screen.height - y);
+			;
+			return new Rect(startPos, new Vector2(_width, 1.0f));
 		}
 
+		Vector2 CalculateScreenPosFromWorldPos(Vector3 position)
+		{
+			Vector2 screenPoint = Camera.main.WorldToScreenPoint(position);
+			screenPoint.y = Screen.height - screenPoint.y;
+			return screenPoint;
+		}
+		float CalculateRotationAngle(Vector2 widgetPos, Vector2 anchorPos)
+		{
+			var toPos = (widgetPos - anchorPos).normalized;
+			var upAxis = Vector2.up;
+			// var dotP = Vector2.Dot(Vector2.up, toPos);
+			return Mathf.Rad2Deg * (Mathf.Atan2(toPos.y, toPos.x) - Mathf.Atan2(Vector2.up.y, Vector2.up.x));
+		}
+		#endregion
 		public void ShowArrow(TimelineActionWidget widget, GameObject highlightAnchor)
 		{
-			ShowArrow(widget.transform.position, highlightAnchor.transform.position);
+			_widget = widget;
+			_anchor = highlightAnchor;
+			// ShowArrow(widget.transform.position, highlightAnchor.transform.position);
+			// _showing = true;
 		}
 
 		public void ShowArrow(Vector3 start, Vector3 end)
